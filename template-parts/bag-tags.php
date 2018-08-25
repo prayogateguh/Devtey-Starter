9f14b66537a3df2aef0ef1127f04954327f4591c
Prayoga Teguh <teguh.p@students.amikom.ac.id>
2018-08-26 06:57:50 +0700

    tags bugs pada single page & attachment (terlalu banyak, dilimit jadi 100)

--- template-parts/bag-tags.php
+++ template-parts/bag-tags.php
@@ -1,6 +1,6 @@
 <div class="tags" style="margin-bottom:40px;">
     <?php 
-    $tags = get_tags(); 
+    $tags = get_tags(array('number' => 100, 'orderby' => 'count', 'order' => 'DESC')); 
 
     foreach ($tags as $tag) { ?>
         <a style="font-size:130%;" href="<?php echo get_bloginfo('url'); ?>/tag/<?php echo $tag->slug; ?>/"><?php echo $tag->name; ?></a>
